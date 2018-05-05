d6565826e30f29f2a2d5d5d907e21e50922d6195
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2018-05-05 17:55:46 +0300

    add backend configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,10 @@
 import createApp from './app';
+import config from '../config/app';
 import logger from './utils/logger';
 
+const { port } = config;
+
 const app = createApp();
-const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   logger.info(`Server is listening on port ${port}`);
