361bb8781001b9ab476aaff1ed3aff22457c493e
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2018-05-13 13:19:55 +0300

    add link for WebSocket

---
+++ src/ssh/ws.ts
@@ -0,0 +1,44 @@
+import * as logger from '../utils/logger';
+
+function attachStreamToWebSocket(stream, ws) {
+  const onStreamData = data => {
+    ws.send(String(data));
+  };
+
+  const onStreamClose = code => {
+    logger.info('Closing WebSocket on process exit code %d', code);
+    ws.terminate();
+  };
+
+  const onMessage = raw => {
+    const { type, ...message } = JSON.parse(raw);
+
+    switch (type) {
+      case 'input': {
+        const { data } = message;
+        stream.write(data);
+        break;
+      }
+
+      case 'resize': {
+        const { size: { rows, cols } } = message;
+        stream.setWindow(rows, cols);
+        break;
+      }
+
+      default:
+        logger.warn(`Unknown message type - ${type}`);
+    }
+  };
+
+  const onClose = () => {
+    logger.info('Client disconnected');
+    stream.removeListener('data', onStreamData);
+  };
+
+  stream.on('data', onStreamData);
+  stream.once('close', onStreamClose);
+
+  ws.on('message', onMessage);
+  ws.once('close', onClose);
+}
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,6 +4,10 @@ export function info(message: string, ...args: any[]) {
   winston.info(message, ...args);
 }
 
+export function warn(message: string, ...args: any[]) {
+  winston.warn(message, ...args);
+}
+
 export function error(message: string, ...args: any[]) {
   winston.error(message, ...args);
 }
