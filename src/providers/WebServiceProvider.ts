fcb44d74d20e67ca311f721b55f5cd68d23dd8bb
Roman <roman.lakhtadyr@gmail.com>
2018-05-31 23:26:22 +0300

    remove CORS as unnecessary

--- src/providers/WebServiceProvider.ts
+++ src/providers/WebServiceProvider.ts
@@ -1,13 +1,10 @@
 import { Container } from 'inversify';
 import * as Koa from 'koa';
-import * as cors from '@koa/cors';
 import * as websockify from 'koa-websocket';
 import { wsRouter, router } from "../routers";
 
 export default (app: Container) => {
   const koa = websockify(new Koa());
-
-  koa.use(cors());
 
   koa.use(router.routes(), router.allowedMethods());
   koa.ws.use(wsRouter.routes(), wsRouter.allowedMethods());
