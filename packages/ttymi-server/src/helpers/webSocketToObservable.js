4212c6fe7ef580c75ad7674b2425f83b35b4c9d9
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2018-09-24 19:30:39 +0300

    Update webSocketToObservable.js

--- packages/ttymi-server/src/helpers/webSocketToObservable.js
+++ packages/ttymi-server/src/helpers/webSocketToObservable.js
@@ -3,10 +3,16 @@ import type WebSocket from 'ws';
 import { Observable, type Observer, Subject } from 'rxjs';
 
 export function websocketToObservable(ws: WebSocket, input$: Subject<string>): Observable<string> {
-  input$.subscribe(data => ws.send(data));
+  const subscription = input$.subscribe(data => ws.send(data));
   return Observable.create((observer: Observer<string>) => {
     ws.on('message', msg => observer.next(msg));
-    ws.on('close', () => observer.complete());
-    ws.on('error', err => observer.error(err));
+    ws.on('close', () => {
+      subscription.unsubscribe();
+      observer.complete();
+    });
+    ws.on('error', err => {
+      subscription.unsubscribe();
+      observer.error(err);
+    });
   });
 }
