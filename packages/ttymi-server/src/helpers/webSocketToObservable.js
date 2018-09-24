0e0fbf01c4ecd12d3b403daae263b5a59fc275e8
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2018-09-24 21:35:24 +0300

    Improve helper tests

--- packages/ttymi-server/src/helpers/webSocketToObservable.js
+++ packages/ttymi-server/src/helpers/webSocketToObservable.js
@@ -1,9 +1,13 @@
 // @flow strict
 import type WebSocket from 'ws';
-import { Observable, type Observer, Subject } from 'rxjs';
+import { Observable, Observer, Subject } from 'rxjs';
 
-export function websocketToObservable(ws: WebSocket, input$: Subject<string>): Observable<string> {
-  const subscription = input$.subscribe(data => ws.send(data));
+export function websocketToObservable(
+  ws: WebSocket,
+  writable$: Subject<string>,
+): Observable<string> {
+  const subscription = writable$.subscribe(data => ws.send(data));
+
   return Observable.create((observer: Observer<string>) => {
     ws.on('message', msg => observer.next(msg));
     ws.on('close', () => {
